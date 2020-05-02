57d771d0e3ede4a16fcfcc8af5f1700872b1ab37
snow0112 <hsuehweh@usc.edu>
2020-05-02 06:39:39 -0700

    clean the mess

--- main.py
+++ main.py
@@ -40,11 +40,7 @@ class MyQtApp(multimediaUI.Ui_MainWindow, QtWidgets.QMainWindow):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("Multimedia Project")
-        #self.import_PB.clicked.connect(self.openFile)
-        #self.toolButton.clicked.connect(self.openFile)
 
-        #self.folderName = "../../576RGBVideo1/"
-        # self.folderName = "/Users/luckyjustin/Documents/JustinProject/576Project/CSCI576ProjectMedia/576RGBVideo1/"
         self.play_btn.clicked.connect(self.play)
         self.pause_btn.clicked.connect(self.pause)
         self.stop_btn.clicked.connect(self.stop)
@@ -55,7 +51,6 @@ class MyQtApp(multimediaUI.Ui_MainWindow, QtWidgets.QMainWindow):
         synopsis = readrgb.readrgbtoQImage("test.rgb", 352*25, 288)
         #print(self.synopsis.size())
         pixmap_syn = QPixmap.fromImage(synopsis).scaled(3000, 1440, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-        #pixmap_syn.scaledToHeight(100)
         self.synopsis.setPixmap(pixmap_syn)
         self.synopsis.mousePressEvent = self.getPos
         #self.total_length = 880 # synopsis from 0 to 880
@@ -66,12 +61,8 @@ class MyQtApp(multimediaUI.Ui_MainWindow, QtWidgets.QMainWindow):
 
         self.soundPlayer = QMediaPlayer(None, QMediaPlayer.LowLatency)
         self.soundPlayer.setAudioRole(2)
-        #self.soundPlayer.setMedia(QMediaContent(QUrl.fromLocalFile("video_1.wav")))
-        # self.soundPlayer.setMedia(QMediaContent(QUrl.fromLocalFile("/Users/luckyjustin/Documents/JustinProject/576Project/CSCI576ProjectMedia/video_1.wav")))
         self.v_thread = Img_Thread(self.updateframe)
         self.v_thread.signal.connect(self.stop)
-
-        
        
     def play(self):
         #print("play")
@@ -81,7 +72,6 @@ class MyQtApp(multimediaUI.Ui_MainWindow, QtWidgets.QMainWindow):
         self.play_btn.setEnabled(False)
         self.pause_btn.setEnabled(True)
         self.stop_btn.setEnabled(True)
-
 
     def updateframe(self):
         #print(self.current_frame)
@@ -121,7 +111,6 @@ class MyQtApp(multimediaUI.Ui_MainWindow, QtWidgets.QMainWindow):
         pixmap_vdo = QPixmap.fromImage(video)
         self.video.setPixmap(pixmap_vdo)
 
-
     def play_video(self):
         self.current_frame = self.start_frame
         self.soundPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(self.audio_file)))
@@ -131,10 +120,12 @@ class MyQtApp(multimediaUI.Ui_MainWindow, QtWidgets.QMainWindow):
     def getfiles(self, idx):
         # for video
         self.folderName = "../../576RGBVideo1/"
+        # self.folderName = "/Users/luckyjustin/Documents/JustinProject/576Project/CSCI576ProjectMedia/576RGBVideo1/"
         self.start_frame = 1
         self.end_frame = 1000
         self.start_time = 1
         self.audio_file = "video_1.wav"
+        #self.audio_file = "/Users/luckyjustin/Documents/JustinProject/576Project/CSCI576ProjectMedia/video_1.wav"
         # for image
         self.fileName = "image-0003.rgb"
         return idx < 1000 # tp = 1: video ; tp = 0: image
@@ -145,25 +136,11 @@ class MyQtApp(multimediaUI.Ui_MainWindow, QtWidgets.QMainWindow):
         tp = self.getfiles(x//1)
         if self.soundPlayer.state() == QMediaPlayer.PlayingState:
             self.v_thread.kill = 1
-            #self.stop()
             time.sleep(0.03) # for racing
         if tp:
             self.play_video()
         else:
             self.show_img()
-        
-        
-    #def openFile(self):
-    #    fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open Movie",
-    #            QDir.homePath())
-    #
-    #    if fileName != '':
-    #        # self.mediaPlayer.setMedia(
-    #        #         QMediaContent(QUrl.fromLocalFile(fileName)))
-    #        self.playButton.setEnabled(True)
-    #        self.pauseButton.setEnabled(True)
-    #        self.stopButton.setEnabled(True)
-
 
 if __name__ == "__main__":
     import sys
@@ -172,5 +149,3 @@ if __name__ == "__main__":
     qt_app = MyQtApp()
     qt_app.show()
     sys.exit(app.exec_())
-
-
